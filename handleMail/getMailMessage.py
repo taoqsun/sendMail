e5775c6f3d9316668ef52d5a21a8964bb4b4544c
taoqsun <taoqsun@TAOQSUN-WS.cisco.com>
2015-09-22 17:16:39 +0800

    commit

--- handleMail/getMailMessage.py
+++ handleMail/getMailMessage.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on Sep 22, 2015
 
@@ -5,8 +6,48 @@ Created on Sep 22, 2015
 '''
 import os
 import sys
+
 try:
     import xlrd
 except Exception,e:
-    print os.path.dirname(__file__)
-    os.system("cd \d "+"&&")
+    print os.path.split(os.path.dirname(__file__))[0]+'\\vendors\\xlrd-0.9.4&&python setup.py install'
+    os.system("cd /d "+os.path.split(os.path.dirname(__file__))[0]+'\\vendors\\xlrd-0.9.4&&python setup.py install')
+    
+class MailMessage(object):
+    
+    def __init__(self):
+        pass
+    
+    def getMailMessage(self):
+        data = xlrd.open_workbook('../vendors/pay.xlsx')
+        table = data.sheets()[0]
+        nrows = table.nrows
+        ncols = table.ncols
+        pepoleList=[]
+          
+        for i in range(nrows):
+            if table.row(i)[2].value != '' :
+                payInfo= table.row_values(i)
+                emailAddress= table.row(i)[2].value
+                pepole=PepoleInfo(emailAddress,payInfo[:])
+                pepoleList.append(pepole)
+        
+        for j in range(len(pepoleList)):
+            
+            print pepoleList[j].getPayInfoParam()
+            print pepoleList[j].getEmailAddressParam()
+                
+class PepoleInfo(object):
+    
+    def __init__(self, emailAddress,payInfo=[]):
+        self.emailAddress = emailAddress
+        self.payInfo = payInfo
+        
+    def getPayInfoParam(self):
+        return self.payInfo
+    
+    def getEmailAddressParam(self):
+        return self.emailAddress
+
+test=MailMessage()
+test.getMailMessage()
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ Created on Sep 22, 2015
 
 @author: sky
 '''
-
+from handleMail.getMailMessage  import *
 
 if __name__ == '__main__':
+    
     pass
