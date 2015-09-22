fca327501e35712c901ea84f8cdfd7e918ab09c7
taoqsun <taoqsun@TAOQSUN-WS..com>
2015-09-22 09:50:36 +0800

    commit

--- handleMail/getMailMessage.py
+++ handleMail/getMailMessage.py
@@ -4,7 +4,9 @@ Created on Sep 22, 2015
 @author: sky
 '''
 import os
+import sys
 try:
     import xlrd
 except Exception,e:
+    print os.path.dirname(__file__)
     os.system("cd \d ")
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ Created on Sep 22, 2015
 @author: sky
 '''
 
+
 if __name__ == '__main__':
     pass
